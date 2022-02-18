88765ccd075136f0161c6e7b60072d8cb47c9ca3
Md.Jubair <you@example.com>
2022-02-18 19:51:05 +0600

    biz

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+/*$('').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    adaptiveHeight: false,
+    arrows:false,
+    autoplay: true,
+    autoplaySpeed: 000,
+  });*/
+
+
+/*$('.icon_1').click(function(){
+  $('.nav_menu').slideToggle();
+})*/
